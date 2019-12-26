d7c6c236f108d5ae569b97e4afbba7c4082a7097
Hesam Asnaashari <ilhesam@outlook.com>
2019-12-26 17:11:31 +0330

    Add project files.

---
+++ SpecFlow.Selenium.xUnit/Features/GoogleSearch.feature
@@ -0,0 +1,9 @@
+﻿Feature: GoogleSearch
+	In order to avoid mistake in search
+
+Scenario: Search Text
+		The text inside the search field does not change
+	Given I going to the "https://google.com" home page
+	When I enter a text "for example" to search
+	And I click on the search button
+	Then The text inside the search field should not be changed
---
+++ SpecFlow.Selenium.xUnit/Steps/GoogleSearchSteps.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace SpecFlow.Selenium.xUnit.Steps
+{
+    [Binding]
+    public class GoogleSearchSteps : TechTalk.SpecFlow.Steps, IDisposable
+    {
+        private readonly IWebDriver _webDriver;
+        private const string TextKey = "text";
+
+        public GoogleSearchSteps()
+        {
+            _webDriver = new ChromeDriver();
+        }
+
+        ~GoogleSearchSteps()
+        {
+            Dispose();
+        }
+
+        [Given(@"I going to the ""(.*)"" home page")]
+        public void GivenIGoingToTheHomePage(string url)
+        {
+            _webDriver.Navigate().GoToUrl(url);
+        }
+
+        [When(@"I enter a text ""(.*)"" to search")]
+        public void WhenIEnterATextToSearch(string text)
+        {
+            var element = _webDriver.FindElement(By.Name("q"));
+            element.SendKeys(text);
+
+            this.ScenarioContext.Add(TextKey, text);
+        }
+
+        [When(@"I click on the search button")]
+        public void WhenIClickOnTheSearchButton()
+        {
+            var element = _webDriver.FindElement(By.XPath("//*[@id='tsf']/div[2]/div[1]/div[3]/center/input[1]"));
+            element.Click();
+        }
+
+        [Then(@"The text inside the search field should not be changed")]
+        public void ThenTheTextInsideTheSearchFieldShouldNotBeChanged()
+        {
+            var element = _webDriver.FindElement(By.Name("q"));
+
+            var actual = element.GetAttribute("value");
+            var expected = this.ScenarioContext.Get<string>(TextKey);
+
+            Assert.Equal(expected, actual);
+        }
+
+        public void Dispose()
+        {
+            _webDriver?.Dispose();
+        }
+    }
+}
